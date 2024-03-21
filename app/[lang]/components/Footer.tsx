bbba8cadf5722984aa311052fe2ab9b275952146
Amir Sheikhmagomedov <amirsheikhmagomedov@gmail.com>
2024-03-21 18:30:50 +0300

    New address

--- app/[lang]/components/Footer.tsx
+++ app/[lang]/components/Footer.tsx
@@ -4,9 +4,11 @@ import { getDictionary } from '@/lib/dictionary'
 export default async function Footer({ lang }: { lang: Locale }) {
   const { footer } = await getDictionary(lang)
 
+  const currentYear = new Date().getFullYear()
+
   return (
     <footer className="bg-blue-text text-white w-[100%] max-[1023px]:w-[100vw] h-[48px] flex items-center justify-center max-[460px]:text-[14px] max-[380px]:text-[12px] leading-[100%]">
-      {footer} © 2022–2023 IMPERIAL LLC
+      {footer} © 2022 – {currentYear} IMPERIAL LLC
     </footer>
   )
 }
