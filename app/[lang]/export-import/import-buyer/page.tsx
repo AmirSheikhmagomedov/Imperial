657b3f6502d5a76e35e661029366f864b927aeef
Amir Sheikhmagomedov <amirsheikhmagomedov@gmail.com>
2024-07-11 02:23:18 +0300

    Added page

--- app/[lang]/export-import/import-buyer/page.tsx
+++ app/[lang]/export-import/import-buyer/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next'
+import Link from 'next/link'
 import { Locale } from '@/i18n.config'
 import { getDictionary } from '@/lib/dictionary'
 
@@ -20,17 +21,33 @@ export async function generateMetadata({
 }
 
 export default async function ImportBuyer({
-  params,
+  params: { lang },
 }: {
   params: { lang: Locale }
 }) {
-  const { inDev } = await getDictionary(params.lang)
+  const {
+    page: { importBuyer },
+  } = await getDictionary(lang)
 
   return (
-    <section>
-      <h1 className="text-black text-[48px] font-bold text-center leading-[120%] mb-[16px] max-[398px]:text-[37px]">
-        {inDev}
+    <section className="flex flex-col items-center gap-[32px] pt-[20px]">
+      <h1 className="text-black text-[48px] font-bold text-center leading-[120%]">
+        {importBuyer.title}
       </h1>
+      <div className="flex flex-col items-center gap-[32px] max-w-[440px] max-[360px]:text-[14px] pl-[48px] max-[540px]:pl-[15px]">
+        <p className="text-[18px] self-start mb-[-16px]">
+          <Link
+            className="leading-[160%] font-bold  text-center underline hover:text-blue-text underline-offset-[4px]"
+            href={`https://perevalochny-kompleks.turbo.site/perevalka-gruzov`}
+          >
+            {importBuyer.textLink}
+          </Link>{' '}
+          <br /> {importBuyer.text}
+        </p>
+        <p className="self-start font-bold mb-[-20px]">{importBuyer.one}</p>
+        <p className="self-start font-bold mb-[-20px]">{importBuyer.two}</p>
+        <p className="self-start font-bold mb-[-20px]">{importBuyer.three}</p>
+      </div>
     </section>
   )
 }
