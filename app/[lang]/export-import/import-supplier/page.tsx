fec41e6a5df403e0f4bfa307f1b23a7d11a1eb57
Amir Sheikhmagomedov <amirsheikhmagomedov@gmail.com>
2024-07-29 18:27:17 +0300

    import for supplier page added

--- app/[lang]/export-import/import-supplier/page.tsx
+++ app/[lang]/export-import/import-supplier/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next'
+import Link from 'next/link'
 import { Locale } from '@/i18n.config'
 import { getDictionary } from '@/lib/dictionary'
 
@@ -19,18 +20,52 @@ export async function generateMetadata({
   }
 }
 
-export default async function ImportSupplier({
-  params,
+export default async function ImportBuyer({
+  params: { lang },
 }: {
   params: { lang: Locale }
 }) {
-  const { inDev } = await getDictionary(params.lang)
+  const {
+    page: { importSupplier },
+  } = await getDictionary(lang)
 
   return (
-    <section>
-      <h1 className="text-black text-[48px] font-bold text-center leading-[120%] mb-[16px] max-[398px]:text-[37px]">
-        {inDev}
+    <section className="flex flex-col items-center gap-[32px] pt-[100px] max-w-[600px]">
+      <h1 className="text-black text-[48px] font-bold text-center leading-[120%]">
+        {importSupplier.title}
       </h1>
+      <p className="text-[18px] max-w-[400px] text-center ">
+        {importSupplier.text}
+      </p>
+      <div>
+        <p className="text-[20px] font-bold">
+          {importSupplier.parallelImport.title}
+        </p>
+        <p className="mb-[20px]">{importSupplier.parallelImport.description}</p>
+        <p className="font-bold mb-[12px]">
+          {importSupplier.parallelImport.listTitle}:
+        </p>
+        <div className="mb-[40px]">
+          <p>✅ {importSupplier.parallelImport.listOne}</p>
+          <p>✅ {importSupplier.parallelImport.listTwo}</p>
+          <p>✅ {importSupplier.parallelImport.listThree}</p>
+          <p>✅ {importSupplier.parallelImport.listFour}</p>
+          <p>✅ {importSupplier.parallelImport.listFive}</p>
+          <p>✅ {importSupplier.parallelImport.listSix}</p>
+        </div>
+        <div>
+          <p className="text-[20px] font-bold">
+            {importSupplier.customs.title}
+          </p>
+          <p className="mb-[20px]">{importSupplier.customs.description}</p>
+          <Link
+            className="leading-[160%] font-bold text-[18px] underline hover:text-blue-text underline-offset-[8px]"
+            href={'https://sama-nova.com'}
+          >
+            <p className="text-center">{importSupplier.customs.text}</p>
+          </Link>
+        </div>
+      </div>
     </section>
   )
 }
