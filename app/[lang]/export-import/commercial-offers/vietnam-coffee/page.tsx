a4abbfd0df85870846c47e6310fc4b4c1553e2db
Amir Sheikhmagomedov <amirsheikhmagomedov@gmail.com>
2024-12-16 16:37:52 +0300

    page changes

--- app/[lang]/export-import/commercial-offers/vietnam-coffee/page.tsx
+++ app/[lang]/export-import/commercial-offers/vietnam-coffee/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import { Metadata } from 'next'
 import { Locale } from '@/i18n.config'
 import { getDictionary } from '@/lib/dictionary'
 import SaveButton from '@/app/[lang]/components/SaveButton'
 import ViewButton from '@/app/[lang]/components/ViewButton'
 import coffeeVariants from '@/public/assets/images/coffeeVariants.png'
+import coffeeNews from '@/public/assets/images/coffeeVietnamNews.jpg'
 
 export async function generateMetadata({
   params,
@@ -43,7 +45,7 @@ export default async function FoodProducts({
       <h1 className="text-black max-w-[750px] text-[48px] font-bold text-center leading-[120%] mb-[32px] max-[480px]:text-[36px]">
         {catalog.commercialOffers.coffeeVietnam.title}
       </h1>
-      <div className="flex gap-[24px]">
+      {/*<div className="flex gap-[24px] mb-[40px]">
         <ViewButton
           dictionary={catalog.watch}
           className="self-start"
@@ -54,7 +56,35 @@ export default async function FoodProducts({
           path="/documents/commercial-offers/coffeeVietnam/document.pdf"
           fileName={`${catalog.commercialOffers.coffeeVietnam.title}.pdf`}
         />
-      </div>
+      </div>*/}
+      <Image
+        src={coffeeNews}
+        width={650}
+        className="mb-[40px] border-[1px] border-black rounded"
+        alt="coffee"
+      />
+      <p className="text-black text-[16px] max-w-[650px] mb-[20px]">
+        {catalog.commercialOffers.coffeeVietnam.textOne}
+      </p>
+      <p className="text-black text-[16px] max-w-[650px] mb-[40px]">
+        {catalog.commercialOffers.coffeeVietnam.textTwo}
+      </p>
+      <Link
+        href={
+          'http://localhost:3000/assets/documents/commercial-offers/coffeeVietnam/offerForRussia.pdf'
+        }
+        className="font-bold text-[24px] text-center underline leading-[160%] hover:text-blue-text underline-offset-[4px] mb-[20px]"
+      >
+        {catalog.commercialOffers.coffeeVietnam.offerRussia}
+      </Link>
+      <Link
+        href={
+          'http://localhost:3000/assets/documents/commercial-offers/coffeeVietnam/offerForTurkey.pdf'
+        }
+        className="font-bold text-[24px] text-center underline leading-[160%] hover:text-blue-text underline-offset-[4px]"
+      >
+        {catalog.commercialOffers.coffeeVietnam.offerTurkey}
+      </Link>
     </section>
   )
 }
