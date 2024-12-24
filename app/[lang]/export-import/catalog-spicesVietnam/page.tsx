a34248cc338c5fc9ea0040ce85ee3ae3d080c8ea
Amir Sheikhmagomedov <amirsheikhmagomedov@gmail.com>
2024-12-24 19:47:01 +0300

    Update page.tsx

--- app/[lang]/export-import/catalog-spicesVietnam/page.tsx
+++ app/[lang]/export-import/catalog-spicesVietnam/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next'
+import Link from 'next/link'
 import { Locale } from '@/i18n.config'
 import { getDictionary } from '@/lib/dictionary'
 import SaveButton from '@/app/[lang]/components/SaveButton'
@@ -27,6 +28,7 @@ export default async function FoodProducts({
   const {
     page: { catalog },
     navigation,
+    getOfferLink,
   } = await getDictionary(lang)
 
   return (
@@ -34,7 +36,7 @@ export default async function FoodProducts({
       <h1 className="text-black text-[48px] font-bold text-center leading-[120%] mb-[48px]">
         {navigation.subMenu2.spicesVietnam}
       </h1>
-      <div className="flex gap-[24px]">
+      <div className="flex gap-[24px] mb-[40px]">
         <ViewButton
           dictionary={catalog.watch}
           className="self-start"
@@ -46,6 +48,12 @@ export default async function FoodProducts({
           fileName={`${navigation.subMenu2.spicesVietnam}.pdf`}
         />
       </div>
+      <Link
+        href={`/${lang}/write-us`}
+        className="leading-[160%] font-bold text-[20px]  text-center underline hover:text-blue-text underline-offset-[4px]"
+      >
+        {getOfferLink}
+      </Link>
     </section>
   )
 }
